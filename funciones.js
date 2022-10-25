73a838556f0d95a39ac86b58e55f588a6457545c
miguel <miguel10rsa@hotmail.com>
2022-10-25 17:20:15 -0400

    first commit

---
+++ funciones.js
@@ -0,0 +1,13 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+
+btn1.addEventListener("click", function() {
+    let bs = document.getElementById("bs").value;
+    let dol = document.getElementById("dol");
+    dol.value = bs / 6.98;
+});
+btn2.addEventListener("click", function() {
+    let bs = document.getElementById("bs");
+    let dol = document.getElementById("dol").value;
+    bs.value = dol / 0.14;
+});
